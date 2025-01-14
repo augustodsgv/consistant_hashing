b3d347233d49f25ffcfdf7f0ae648bfe852d3553
augusto.vaz <augusto.vaz@luizalabs.com>
2025-01-14 08:44:50 -0300

    refactor: make use of environment variables to max_hash_size

--- src/hash.py
+++ src/hash.py
@@ -1,8 +1,13 @@
 import hashlib
-MAX_HASH_SIZE = 100
+import os
 
 def hash(key: str)->int:
     """
     returns the md5 hash of the key
     """
-    return int(hashlib.md5(key.encode()).hexdigest(), 16) % MAX_HASH_SIZE
+    hash_value = int(hashlib.md5(key.encode()).hexdigest(), 16)
+    if 'MAX_HASH_SIZE' in os.environ:
+        MAX_HASH_SIZE = int(os.environ['MAX_HASH_SIZE'])
+        return hash_value % MAX_HASH_SIZE
+    return hash_value
+    
